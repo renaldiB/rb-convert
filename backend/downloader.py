d81cd808ef6a19ca0a6b69200b58f436d1909041
RB <renaldibong@gmail.com>
2026-09-14 00:13:59 +0700

    fix(youtube): use bestvideo* for progressive stream matching in quality selector

--- backend/downloader.py
+++ backend/downloader.py
@@ -1169,7 +1169,7 @@ def download_media_file_ytdlp(url: str, format_type: str, platform: str, downloa
     # 2. Audio MP3 Format: Ensure audio-capable streams are selected across all platforms
     if format_type == "mp3":
         if platform == "youtube":
-            fmt_selector = '18/bestaudio/bestaudio*/best[acodec!=none]/best'
+            fmt_selector = 'bestaudio/bestaudio*/18/best[acodec!=none]/best'
         elif platform == "instagram":
             fmt_selector = 'bestaudio/bestaudio*/best[acodec!=none]/1/2/3'
         else:
@@ -1183,14 +1183,14 @@ def download_media_file_ytdlp(url: str, format_type: str, platform: str, downloa
     elif format_type == "mp4":
         if quality and quality.isdigit():
             if platform == "youtube":
-                fmt_selector = f'bestvideo[height<={quality}]+bestaudio/best[height<={quality}]/18/best'
+                fmt_selector = f'bestvideo*[height<={quality}]+bestaudio/best[height<={quality}]/18/best'
             elif platform == "instagram":
                 fmt_selector = f'bestvideo[height<={quality}]+bestaudio/best[height<={quality}][acodec!=none]/1/2/3/bestvideo+bestaudio'
             else:
                 fmt_selector = f'bestvideo[height<={quality}]+bestaudio/best[height<={quality}][acodec!=none]/bestvideo+bestaudio'
         else:
             if platform == "youtube":
-                fmt_selector = '18/bestvideo*+bestaudio/bestvideo*+bestaudio*/best'
+                fmt_selector = 'bestvideo*+bestaudio/bestvideo*+bestaudio*/18/best'
             elif platform == "instagram":
                 fmt_selector = 'bestvideo+bestaudio/bestvideo*+bestaudio*/best[acodec!=none]/1/2/3'
             else:
